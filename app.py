cade08cfd5bccb5c060145b9ba85fbf4868fea30
vignesh rajkumar <vigneshrajkumar@hotmail.com>
2019-08-01 15:25:45 +0530

    gzip compression handing

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from article import Article
 from source import Source
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import gzip
 
 def main():
   print("News Mine")
@@ -11,17 +12,26 @@ def main():
   s1 = Source("The News Minute", "tit", "aut", "201", "the")
   print(s1)
 
-  page = urlopen("https://www.thenewsminute.com/article/rare-surgery-chennai-doctors-extract-526-teeth-7-year-olds-mouth-106448").read()
-  
-  print(page)
+  page = urlopen("https://www.thenewsminute.com/article/rare-surgery-chennai-doctors-extract-526-teeth-7-year-olds-mouth-106448")
+
+  content = ""
+
+  if page.info().get('Content-Encoding') == 'gzip':
+      print("gzip comp")
+
+      gzipFile = gzip.GzipFile(fileobj=page)
+      content = gzipFile.read()
+  else:
+      print("nah")
+      content = page.read()
+
 
   
-  
-#   print(page.read()
 
-#   soup = BeautifulSoup(page, "html.parser")
 
-#   print(soup)
+  soup = BeautifulSoup(content, "html.parser")
+
+  print(soup.h1)
 
 
 
